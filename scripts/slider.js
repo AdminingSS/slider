bb8a4794df75d037091b3d8e9e3f1cae1f7e4bd0
AdminingSS <admining@mail.ru>
2018-10-08 20:57:12 +0300

    2

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,22 +1,44 @@
 class Slider {
+
+    //options: {}
+    //options.id - id элемента
+    //options.css - css селектор элемента
+    //options.elem - переданный элемент
+
     constructor (id) {
-        const sliderElem = document.getElementById(id);
-        sliderElem.classList.add('slider-gallery');
 
-        const slideElems = sliderElem.children;
-        alert(slideElems.length);
-
-        const wrapper = document.createElement('div');
-        wrapper.classList.add('slider-slides');
-
-        for(const child of slideElems) {
-            child.classList.add('slider-slide');
-            wrapper.appendChild(child);
+        if(typeof(id) === "string") {
+            this.sliderID = id;
+        }
+        else {
+            this.sliderID = id.id;
         }
 
-        sliderElem.appendChild(wrapper);
+        this.elem = document.getElementById(this.sliderID) || document.querySelector(id.css) || id.elem;
 
-        let currentSlide = 0;
+        this.sliderClass = 'slider-gallery';
+        this.slidesContainerClass = 'slider-slides';
+        this.slideClass = 'slider-slide';
 
+        this.currentSlide = 0;
+
+        this.init();
+    }
+
+    init() {
+
+        this.elem.classList.add(this.sliderClass);
+
+        const slideElems = this.elem.children;
+
+        const wrapper = document.createElement('div');
+        wrapper.classList.add(this.slidesContainerClass);
+
+        while(slideElems.length) {
+            slideElems[0].classList.add(this.slideClass);
+            wrapper.appendChild(slideElems[0]);
+        }
+
+        this.elem.appendChild(wrapper);
     }
 }
