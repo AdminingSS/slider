83b87297a75f9df27ac336aceee390138719356b
AdminingSS <admining@mail.ru>
2019-07-05 21:27:24 +0300

    7

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
-const options = {};
+const options = {
+    slidesToShow: 3,
+    slideMargin: 20,
+    responsiveOptions: [
+        {
+            breakpoint: 960,
+            slidesToShow: 1,
+            slideMargin: 0
+        }
+    ]
+};
 options.elem = document.getElementById('slider');
 
 const slider = new Slider(options);
--- scripts/slider.js
+++ scripts/slider.js
@@ -17,12 +17,40 @@ class Slider {
         
         this.currentSlide = 0;
         this.slidesCount = 0;
+        this.currentWidth = 0;
+        this.baseSlidesToShow = options.slidesToShow || 1;
+        this.baseSlideMargin = options.slideMargin || 0;
+        // this.slidesToShow = options.slidesToShow || 1;
+        // this.slideMargin = options.slideMargin || 0;
+        this.leftBlocked = false;
+        this.rightBlocked = false;
+        this.responsiveOptions = options.responsiveOptions || [];
         //this.slidesOffsets = [];
 
         this.init();
+        this.reInit();
     }
 
     init() {
+
+        this.slidesToShow = this.baseSlidesToShow;
+        this.slideMargin = this.baseSlideMargin;
+
+        if(this.responsiveOptions.length) {
+            const optionsCounter = this.responsiveOptions.length;
+            for(let i = 0; i < optionsCounter; i++) {
+                if(this.checkResponsive(this.responsiveOptions[i].breakpoint)) {
+                    this.slidesToShow = this.responsiveOptions[i].slidesToShow;
+                    this.slideMargin = this.responsiveOptions[i].slideMargin;
+                }
+            }
+        }
+
+        const totalVisibleMargins = this.slideMargin * (this.slidesToShow -1);
+
+        this.currentWidth = this.elem.offsetWidth || this.elem.getBoundingClientRect().width;
+
+        const slideWidth = (this.currentWidth - totalVisibleMargins) / this.slidesToShow;
 
         this.elem.classList.add(this.sliderClasses.main);
 
@@ -34,10 +62,15 @@ class Slider {
 
         while(slideElems.length) {
             slideElems[0].classList.add(this.sliderClasses.slide);
+            slideElems[0].style.width = slideWidth + 'px';
+            slideElems[0].style.marginRight = this.slideMargin + 'px';
             this.wrapper.appendChild(slideElems[0]);
         }
 
         this.elem.appendChild(this.wrapper);
+
+        if(this.currentSlide === 0) this.leftBlocked = true;
+        if(this.currentSlide === slideElems.length - 1 ) this.rightBlocked = true;
 
         const prevArrow = document.createElement('div');
         prevArrow.classList.add('prev-arrow');
@@ -56,17 +89,19 @@ class Slider {
         this.showSlide(this.currentSlide);
 
         this.elem.addEventListener('click', this.eventHandler.bind(this));
+
+        window.addEventListener('resize', this.reInit.bind(this))
     }
 
     eventHandler (event) {
         let target = event.target;
 
         while (target != this.elem) {
-            if (target.classList.contains('prev-arrow')) {
+            if (target.classList.contains('prev-arrow') && !this.leftBlocked) {
                 this.prevSlide();
                 return;
             }
-            if (target.classList.contains('next-arrow')) {
+            if (target.classList.contains('next-arrow') && !this.rightBlocked) {
                 this.nextSlide();
                 return;
             }
@@ -75,10 +110,23 @@ class Slider {
     }
 
     showSlide(number) {
+        if(number) {
+            this.leftBlocked = false;
+        } else {
+            this.leftBlocked = true;
+        }
+        if(number >= (this.slidesCount - this.slidesToShow)) {
+            this.rightBlocked = true;
+            number = this.slidesCount - this.slidesToShow;
+        } else {
+            this.rightBlocked = false;
+        }
+
         const newLeft = -this.wrapper.children[number].offsetLeft;
         this.wrapper.style.left = newLeft + 'px';
-        const newWidth = this.wrapper.children[number].offsetWidth || this.wrapper.children[number].getBoundingClientRect().width;
-        if (newWidth) this.elem.style.width = newWidth + 'px';
+
+        //const newWidth = this.wrapper.children[number].offsetWidth || this.wrapper.children[number].getBoundingClientRect().width;
+        //if (newWidth) this.elem.style.width = newWidth + 'px';
     }
 
     prevSlide() {
@@ -89,6 +137,67 @@ class Slider {
     nextSlide() {
         this.currentSlide = Math.min(this.currentSlide + 1, this.slidesCount - 1);
         this.showSlide(this.currentSlide);
+    }
+
+    reInit() {
+
+        this.slidesToShow = this.baseSlidesToShow;
+        this.slideMargin = this.baseSlideMargin;
+
+        if(this.responsiveOptions.length) {
+            const optionsCounter = this.responsiveOptions.length;
+            for(let i = 0; i < optionsCounter; i++) {
+                if(this.checkResponsive(this.responsiveOptions[i].breakpoint)) {
+                    this.slidesToShow = this.responsiveOptions[i].slidesToShow;
+                    this.slideMargin = this.responsiveOptions[i].slideMargin;
+                }
+            }
+        }
+
+        const totalVisibleMargins = this.slideMargin * (this.slidesToShow -1);
+
+        this.currentWidth = this.elem.offsetWidth || this.elem.getBoundingClientRect().width;
+
+        const slideWidth = (this.currentWidth - totalVisibleMargins) / this.slidesToShow;
+
+        const slideElems = this.wrapper.children;
+
+        //wrap to div
+
+        let counter = slideElems.length - 1;
+
+        while(counter + 1) {
+            slideElems[counter].style.width = slideWidth + 'px';
+            counter--;
+        }
+
+        // const prevArrow = document.createElement('div');
+        // prevArrow.classList.add('prev-arrow');
+        // this.elem.appendChild(prevArrow);
+        //
+        // const nextArrow = document.createElement('div');
+        // nextArrow.classList.add('next-arrow');
+        // this.elem.appendChild(nextArrow);
+
+        //this.slidesCount = this.wrapper.children.length;
+
+        // for(let i = 0; i < this.slidesCount; i++) {
+        //     this.slidesOffsets[i] = this.wrapper.children[i].offsetLeft;
+        // }
+
+        this.showSlide(this.currentSlide);
+
+        //this.elem.addEventListener('click', this.eventHandler.bind(this));
+    }
+
+    // makeResponsive() {
+    //     this.responsiveOptions.forEach(function (options) {
+    //
+    //     });
+    // }
+
+    checkResponsive(breakpoint) {
+        return window.innerWidth < breakpoint;
     }
 
 }
